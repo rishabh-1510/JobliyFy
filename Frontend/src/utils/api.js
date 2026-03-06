31ce77c29479668987652248f4e8fa8a48317e9b
rishabh-1510 <158290586+rishabh-1510@users.noreply.github.com>
2026-03-07 00:30:42 +0530

    ..

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -3,4 +3,4 @@ const BASE_URL = import.meta.env.BASE_URL;
 export const USER_API_ENDPOINT = `${BASE_URL}/api/v1/user`;
 export const JOB_API_ENDPOINT = `${BASE_URL}/api/v1/job`;
 export const APPLICATION_API_ENDPOINT = `${BASE_URL}/api/v1/application`;
-export const COMPANIES_API_ENDPOINTS = `${BASE_URL}/api/v1/company`;     
+export const COMPANIES_API_ENDPONTS= `${BASE_URL}/api/v1/company`;     
