5e48d229beb4919f35615739f35a7d329e5a0469
rishabh-1510 <158290586+rishabh-1510@users.noreply.github.com>
2026-03-07 01:33:58 +0530

    .....

--- Frontend/src/components/admin/ProtectedRoute.jsx
+++ Frontend/src/components/admin/ProtectedRoute.jsx
@@ -2,17 +2,6 @@ import { useSelector } from "react-redux";
 import { Navigate, useLocation } from "react-router-dom";
 import store from "../../redux/store";
 const ProtectedRoute = ({ children }) => {
-  const { user } = useSelector((store) => store.auth);
-  const location = useLocation();
-
-  if (!user || user.role !== "recruiter") {
-    // prevent redirect loop
-    if (location.pathname !== "/login") {
-      return <Navigate to="/login" replace />;
-    }
-    return null;
-  }
-
   return children;
 };
 
