a8dd72e4ee78a789ba1fe8d2e31c1b93185ed6e5
rishabh-1510 <158290586+rishabh-1510@users.noreply.github.com>
2026-03-07 01:10:12 +0530

    ....

--- Frontend/src/hooks/useGetAllCompanies.jsx
+++ Frontend/src/hooks/useGetAllCompanies.jsx
@@ -1,27 +1,31 @@
-import axios from 'axios'
-import React, { useEffect } from 'react'
-import {COMPANIES_API_ENDPONTS} from '../utils/api'
-import { useDispatch, useSelector } from 'react-redux'
-import {  setCompanies} from '../redux/companySlice'
-import store from '../redux/store'
-const useGetAllCompanies = (companyId) => {
-    const {user} = useSelector(store=>store.auth);
-     
-    const dispatch = useDispatch();
-    useEffect(()=>{
-        const fetchCompanies = async()=>{
-            try {
-                const res = await axios.get(`${COMPANIES_API_ENDPONTS}/getcompanies`,{withCredentials:true});
-                if(res.data.success){
-                    dispatch(setCompanies(res.data.companies))
-                }
-                console.log('res is',res)
-            } catch (error) {
-                console.log(error)
-            }
-        }
-        fetchCompanies();
-    },[companyId,dispatch])
-}
+import axios from "axios";
+import { useEffect } from "react";
+import { COMPANIES_API_ENDPOINT } from "../utils/api";
+import { useDispatch } from "react-redux";
+import { setCompanies } from "../redux/companySlice";
 
-export default useGetAllCompanies
+const useGetAllCompanies = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const fetchCompanies = async () => {
+            try {
+                const res = await axios.get(
+                    `${COMPANIES_API_ENDPOINT}/getcompanies`,
+                    { withCredentials: true }
+                );
+
+                if (res.data.success) {
+                    dispatch(setCompanies(res.data.companies));
+                }
+
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        fetchCompanies();
+    }, [dispatch]);
+};
+
+export default useGetAllCompanies;
